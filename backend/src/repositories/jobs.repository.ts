b13c2a8eeeb74281b038357b21336c0d9873aded
Deepesh Gaharwar <deepeshgaharwar3@gmail.com>
2026-09-15 21:38:12 +0530

    feat: added jobs repository

---
+++ backend/src/repositories/jobs.repository.ts
@@ -0,0 +1,72 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Job } from "../entities/job.entity.js"
+import { JobStatus } from "../enums/job_status.enum.js";
+
+@Injectable()
+export class JobsRepository {
+  constructor(
+    @InjectRepository(Job)
+    private readonly jobRepository: Repository<Job>,
+  ) {}
+
+  async create(title: string, type: string): Promise<Job> {
+    const job = this.jobRepository.create({
+      title,
+      type,
+      status: JobStatus.PENDING,
+    });
+
+    return this.jobRepository.save(job);
+  }
+
+  async findAll(
+    skip: number,
+    take: number,
+    status?: JobStatus,
+  ): Promise<[Job[], number]> {
+    const query = this.jobRepository
+      .createQueryBuilder('job')
+      .orderBy('job.createdAt', 'DESC')
+      .skip(skip)
+      .take(take);
+
+    if (status) {
+      query.andWhere('job.status = :status', { status });
+    }
+
+    return query.getManyAndCount();
+  }
+
+  async findById(id: string): Promise<Job | null> {
+    return this.jobRepository.findOne({
+      where: { id },
+    });
+  }
+
+  async updateStatus(
+    id: string,
+    currentStatus: JobStatus,
+    newStatus: JobStatus,
+  ): Promise<boolean> {
+    const result = await this.jobRepository
+      .createQueryBuilder()
+      .update(Job)
+      .set({
+        status: newStatus,
+      })
+      .where('id = :id', { id })
+      .andWhere('status = :currentStatus', { currentStatus })
+      .execute();
+
+    return result.affected === 1;
+  }
+
+  async delete(id: string): Promise<boolean> {
+    const result = await this.jobRepository.delete(id);
+
+    return result.affected === 1;
+  }
+}

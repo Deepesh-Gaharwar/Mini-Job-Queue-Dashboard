6f0ef9f3bd50eaabaf2a36e71ce8276461df8a76
Deepesh Gaharwar <deepeshgaharwar3@gmail.com>
2026-09-16 01:07:57 +0530

    feat: persist redux store with redux-persist

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
-import { store } from './redux/store';
+import { persistor, store } from './redux/store';
 
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor} loading={null}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>,
 );
